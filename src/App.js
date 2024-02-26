6b3fd1313440ae0a8a69f308cb65f273964f9842
Synavos <synavos@Synavoss-MacBook-Pro.local>
2024-02-26 16:34:26 +0500

    added product list

--- src/App.js
+++ src/App.js
@@ -2,18 +2,34 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
 
+  const ProductCard = () => {
+    return (
+      <div className='flex justify-center mt-32'>
+        
+      <div className='bg-[#1c1816] relative w-72 flex flex-col px-3 pt-2 rounded-lg'> 
+       <div>
+          <img src='/zinger.png' className=''/>
+       </div>
+       <h5 className='font-semibold text-white'>Kruch Burger</h5>
+       <p className='mt-3 text-white'>Tender boneless strips, sliced onions, tangy imli chutney, mint</p>
+       <span className='font-bold mt-3 pb-10 text-white	'>
+          Rs 200
+       </span>
+       <button className='bg-[#ea002a] absolute -bottom-5 left-20 hover:bg-white rounded-lg p-1'>
+          <span className='text-xs whitespace-nowrap font-extrabold pb-8 cursor-pointer hover:text-[#ea002a]'> + ADD TO BUCKET</span>
+       </button>
+      </div>
+      </div>
+  
+    )
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Counter PWA with React</p>
-        <div>
-          <button onClick={() => setCount(count - 1)}>-</button>
-          <span style={{ margin: '0 10px' }}>{count}</span>
-          <button onClick={() => setCount(count + 1)}>+</button>
-        </div>
-      </header>
+    <div className='lg:flex gap-10 px-5 flex-wrap justify-center'>
+     {[...Array(10)].map((_, index) => (
+      <ProductCard key={index} />
+    ))}
     </div>
   );
 }
