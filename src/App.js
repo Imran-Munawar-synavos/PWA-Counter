d4ce638f33261ba97719411a0770d2967064cc5f
Imran <imran.munawar@synavos.com>
2024-03-18 12:13:30 +0500

    remove color

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,9 +9,13 @@ function App() {
       <header className="App-header">
         <p>Counter PWA with React</p>
         <div>
-          <button style={{padding : 6 ,background: 'green'}} onClick={() => setCount(count - 1)}>-</button>
-          <span style={{ margin: '0 10px' }}>{count}</span>
-          <button style={{padding : 5, background: 'green'}} onClick={() => setCount(count + 1)}>+</button>
+          <button style={{ padding: 6 }} onClick={() => setCount(count - 1)}>
+            -
+          </button>
+          <span style={{ margin: "0 10px" }}>{count}</span>
+          <button style={{ padding: 5 }} onClick={() => setCount(count + 1)}>
+            +
+          </button>
         </div>
       </header>
     </div>
